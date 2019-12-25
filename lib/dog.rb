195f22b0c80110e9fefc9fe1df82c0989ab943f2
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-25 23:05:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,13 @@
 class Dog 
-  def initialize(name, breed)
+  def initialize(name)
     @name = name
-    @breed = breed 
   end 
   
   def name 
-    @
+    @name 
+  end 
+  def breed 
+    @breed 
+  end
   
 end 
